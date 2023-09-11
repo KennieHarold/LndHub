c881c0d6b991377925e8a2c1ae798e8ddef5fb8b
kennie <kennie.harold@gmail.com>
2023-09-11 19:25:54 +0800

    chore: added dotenv

--- config.js
+++ config.js
@@ -1,28 +1,22 @@
+require("dotenv").config();
+
 let config = {
   enableUpdateDescribeGraph: false,
   postRateLimit: 100,
   rateLimit: 200,
-  forwardReserveFee: 0.01, // default 0.01
-  intraHubFee: 0.003, // default 0.003
-  bitcoind: {
-    rpc: 'http://login:password@1.1.1.1:8332/wallet/wallet.dat',
-  },
+  forwardReserveFee: 0.01,
+  intraHubFee: 0.003,
   redis: {
     port: 12914,
-    host: '1.1.1.1',
+    host: "127.0.0.1",
     family: 4,
-    password: 'password',
+    password: process.env.REDIS_PASSWORD,
     db: 0,
   },
   lnd: {
-    url: '1.1.1.1:10009',
-    password: '',
+    url: process.env.LND_RPC_URL,
+    password: process.env.LND_RPC_PASSWORD,
   },
 };
-
-if (process.env.CONFIG) {
-  console.log('using config from env');
-  config = JSON.parse(process.env.CONFIG);
-}
 
 module.exports = config;
